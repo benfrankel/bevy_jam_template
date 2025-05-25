5a1bbb4c33346934fe62f58a306e2c91eb9f08c1
Ben Frankel <ben.frankel7@gmail.com>
2025-05-24 17:01:02 -0700

    Reorder pause menu buttons

--- src/menu/pause.rs
+++ src/menu/pause.rs
@@ -15,9 +15,9 @@ fn spawn_pause_menu(mut commands: Commands, menu_root: Res<MenuRoot>) {
         .with_child(widget::body(children![
             widget::header("[b]Game paused"),
             widget::column_of_buttons(children![
-                widget::wide_button("Settings", open_settings),
-                widget::wide_button("Continue", disable_menu),
+                widget::wide_button("Continue", close_menu),
                 widget::wide_button("Restart", restart_game),
+                widget::wide_button("Settings", open_settings),
                 widget::wide_button("Quit to title", quit_to_title),
             ])
         ]));
@@ -27,7 +27,7 @@ fn open_settings(_: Trigger<Pointer<Click>>, mut menu: ResMut<NextStateStack<Men
     menu.push(Menu::Settings);
 }
 
-fn disable_menu(_: Trigger<Pointer<Click>>, mut menu: NextMut<Menu>) {
+fn close_menu(_: Trigger<Pointer<Click>>, mut menu: NextMut<Menu>) {
     menu.disable();
 }
 
