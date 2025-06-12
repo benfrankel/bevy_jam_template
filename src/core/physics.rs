0f40e015af5879effa35042c03ab1bfeb4744903
Ben Frankel <ben.frankel7@gmail.com>
2025-06-12 02:44:31 -0700

    Disable friction by default

--- src/core/physics.rs
+++ src/core/physics.rs
@@ -3,6 +3,7 @@ use crate::prelude::*;
 pub(super) fn plugin(app: &mut App) {
     app.add_plugins(PhysicsPlugins::default().with_length_unit(PIXELS_PER_METER));
     app.insert_resource(Gravity::ZERO);
+    app.insert_resource(DefaultFriction(Friction::ZERO));
 
     app.add_systems(StateFlush, Pause.on_edge(unpause_physics, pause_physics));
 }
