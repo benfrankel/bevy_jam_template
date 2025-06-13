0d8fae17c0cb9417e15e20c4cc8dcbdcac317067
Ben Frankel <ben.frankel7@gmail.com>
2025-06-12 20:57:22 -0700

    Add SFX volume selector

--- src/core/audio.rs
+++ src/core/audio.rs
@@ -12,6 +12,7 @@ pub(super) fn plugin(app: &mut App) {
 #[reflect(Resource)]
 pub struct AudioSettings {
     pub master_volume: f32,
+    pub sfx_volume: f32,
     pub music_volume: f32,
     pub ui_volume: f32,
 }
@@ -21,6 +22,7 @@ impl Default for AudioSettings {
         Self {
             master_volume: 0.5,
             music_volume: 0.5,
+            sfx_volume: 0.5,
             ui_volume: 0.5,
         }
     }
@@ -30,7 +32,7 @@ impl Configure for AudioSettings {
     fn configure(app: &mut App) {
         app.register_type::<Self>();
         app.init_resource::<Self>();
-        app.configure::<(IsMusicAudio, IsUiAudio)>();
+        app.configure::<(IsMusicAudio, IsSfxAudio, IsUiAudio)>();
         app.add_systems(
             Update,
             apply_audio_settings
@@ -45,6 +47,10 @@ impl AudioSettings {
         Volume::Linear(self.master_volume * self.music_volume)
     }
 
+    pub fn sfx_volume(&self) -> Volume {
+        Volume::Linear(self.master_volume * self.sfx_volume)
+    }
+
     pub fn ui_volume(&self) -> Volume {
         Volume::Linear(self.master_volume * self.ui_volume)
     }
@@ -53,12 +59,25 @@ impl AudioSettings {
 fn apply_audio_settings(
     audio_settings: Res<AudioSettings>,
     music_audio_query: Query<Entity, With<IsMusicAudio>>,
+    sfx_audio_query: Query<Entity, With<IsSfxAudio>>,
     ui_audio_query: Query<Entity, With<IsUiAudio>>,
     mut volume_query: Query<(Option<&mut PlaybackSettings>, Option<&mut AudioSink>)>,
 ) {
     // Apply music volume.
     let volume = audio_settings.music_volume();
     for entity in &music_audio_query {
+        let (playback, sink) = c!(volume_query.get_mut(entity));
+
+        if let Some(mut sink) = sink {
+            sink.set_volume(volume);
+        } else if let Some(mut playback) = playback {
+            playback.volume = volume;
+        }
+    }
+
+    // Apply SFX volume.
+    let volume = audio_settings.sfx_volume();
+    for entity in &sfx_audio_query {
         let (playback, sink) = c!(volume_query.get_mut(entity));
 
         if let Some(mut sink) = sink {
@@ -91,12 +110,33 @@ impl Configure for IsMusicAudio {
     }
 }
 
+#[allow(dead_code)]
 pub fn music_audio(audio_settings: &AudioSettings, handle: Handle<AudioSource>) -> impl Bundle {
     (
         Name::new("MusicAudio"),
+        IsMusicAudio,
         AudioPlayer(handle),
         PlaybackSettings::LOOP.with_volume(audio_settings.music_volume()),
-        IsMusicAudio,
+    )
+}
+
+#[derive(Component, Reflect, Debug)]
+#[reflect(Component)]
+struct IsSfxAudio;
+
+impl Configure for IsSfxAudio {
+    fn configure(app: &mut App) {
+        app.register_type::<Self>();
+    }
+}
+
+#[allow(dead_code)]
+pub fn sfx_audio(audio_settings: &AudioSettings, handle: Handle<AudioSource>) -> impl Bundle {
+    (
+        Name::new("SfxAudio"),
+        IsSfxAudio,
+        AudioPlayer(handle),
+        PlaybackSettings::LOOP.with_volume(audio_settings.sfx_volume()),
     )
 }
 
@@ -110,13 +150,14 @@ impl Configure for IsUiAudio {
     }
 }
 
+#[allow(dead_code)]
 pub fn ui_audio(audio_settings: &AudioSettings, handle: Handle<AudioSource>) -> impl Bundle {
     (
         Name::new("UiSample"),
+        IsUiAudio,
         AudioPlayer(handle),
         PlaybackSettings::DESPAWN
             .with_volume(audio_settings.ui_volume())
             .with_speed(thread_rng().gen_range(0.9..1.5)),
-        IsUiAudio,
     )
 }
--- src/menu/settings.rs
+++ src/menu/settings.rs
@@ -13,6 +13,7 @@ pub(super) fn plugin(app: &mut App) {
         Settings,
         IsMasterVolumeSelector,
         IsMusicVolumeSelector,
+        IsSfxVolumeSelector,
         IsUiVolumeSelector,
     )>();
 }
@@ -54,6 +55,8 @@ fn grid() -> impl Bundle {
             widget::selector(IsMasterVolumeSelector, master_volume_down, master_volume_up),
             widget::label("Music volume"),
             widget::selector(IsMusicVolumeSelector, music_volume_down, music_volume_up),
+            widget::label("SFX volume"),
+            widget::selector(IsSfxVolumeSelector, sfx_volume_down, sfx_volume_up),
             widget::label("UI volume"),
             widget::selector(IsUiVolumeSelector, ui_volume_down, ui_volume_up),
         ],
@@ -154,6 +157,54 @@ fn music_volume_down(_: Trigger<Pointer<Click>>, mut audio_settings: ResMut<Audi
 
 fn music_volume_up(_: Trigger<Pointer<Click>>, mut audio_settings: ResMut<AudioSettings>) {
     audio_settings.music_volume = (audio_settings.music_volume + 0.1).min(1.0);
+}
+
+#[derive(Component, Reflect, Debug)]
+#[reflect(Component)]
+struct IsSfxVolumeSelector;
+
+impl Configure for IsSfxVolumeSelector {
+    fn configure(app: &mut App) {
+        app.register_type::<Self>();
+        app.add_systems(
+            Update,
+            Menu::Settings.on_update(update_sfx_volume_selector.in_set(UpdateSystems::Update)),
+        );
+    }
+}
+
+fn update_sfx_volume_selector(
+    audio_settings: Res<AudioSettings>,
+    selector_query: Query<Entity, With<IsSfxVolumeSelector>>,
+    children_query: Query<&Children>,
+    mut text_query: Query<&mut RichText>,
+    mut disabled_query: Query<&mut InteractionDisabled>,
+) {
+    for entity in &selector_query {
+        let children = c!(children_query.get(entity))
+            .into_iter()
+            .collect::<Vec<_>>();
+
+        let left = **c!(children.first());
+        c!(disabled_query.get_mut(left)).0 = audio_settings.sfx_volume <= f32::EPSILON;
+
+        let mid = **c!(children.get(1));
+        let mid_children = c!(children_query.get(mid));
+        let label = *c!(mid_children.first());
+        c!(text_query.get_mut(label)).sections =
+            parse_rich(format!("{:.0}%", audio_settings.sfx_volume * 100.0));
+
+        let right = **c!(children.get(2));
+        c!(disabled_query.get_mut(right)).0 = audio_settings.sfx_volume >= 1.0 - f32::EPSILON;
+    }
+}
+
+fn sfx_volume_down(_: Trigger<Pointer<Click>>, mut audio_settings: ResMut<AudioSettings>) {
+    audio_settings.sfx_volume = (audio_settings.sfx_volume - 0.1).max(0.0);
+}
+
+fn sfx_volume_up(_: Trigger<Pointer<Click>>, mut audio_settings: ResMut<AudioSettings>) {
+    audio_settings.sfx_volume = (audio_settings.sfx_volume + 0.1).min(1.0);
 }
 
 #[derive(Component, Reflect, Debug)]
